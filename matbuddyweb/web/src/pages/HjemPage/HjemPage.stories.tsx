2a2961a0f6a5d957697e251dd23bb9165e340782
trymellingsen <trymel@hiof.no>
2025-10-20 15:05:12 +0200

    Setup
    Trym

---
+++ matbuddyweb/web/src/pages/HjemPage/HjemPage.stories.tsx
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import HjemPage from './HjemPage'
+
+const meta: Meta<typeof HjemPage> = {
+  component: HjemPage,
+}
+
+export default meta
+
+type Story = StoryObj<typeof HjemPage>
+
+export const Primary: Story = {}
---
+++ matbuddyweb/web/src/pages/HjemPage/HjemPage.tsx
@@ -0,0 +1,21 @@
+// import { Link, routes } from '@redwoodjs/router'
+import { Metadata } from '@redwoodjs/web'
+
+const HjemPage = () => {
+  return (
+    <>
+      <Metadata title="Hjem" description="Hjem page" />
+
+      <h1>HjemPage</h1>
+      <p>
+        Find me in <code>./web/src/pages/HjemPage/HjemPage.tsx</code>
+      </p>
+      {/*
+          My default route is named `hjem`, link to me with:
+          `<Link to={routes.hjem()}>Hjem</Link>`
+      */}
+    </>
+  )
+}
+
+export default HjemPage
